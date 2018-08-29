feaa91c83b43c04e8a1548771d699b2c41f621a9
Chen Yu Pao <windperson@gmail.com>
2018-08-29 19:09:33 +0800

    Load Embedded SVG file example

--- XamarinFormsSVGdemo/XamarinFormsSVGdemo/MainPage.xaml.cs
+++ XamarinFormsSVGdemo/XamarinFormsSVGdemo/MainPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
 
 namespace XamarinFormsSVGdemo
@@ -12,6 +15,42 @@ namespace XamarinFormsSVGdemo
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void SKCanvasView_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            if (!(sender is SKCanvasView container))
+            {
+                return;
+            }
+
+            SKCanvas canvas = e.Surface.Canvas;
+            canvas.Clear();
+
+            var centerX = (float)container.Width / 2;
+            var centerY = (float)container.Height / 2;
+
+            canvas.Translate(centerX, centerY);
+
+            var scale = (float)(e.Info.Width / container.Width);
+#if DEBUG
+#if !WINDOWS_UWP
+            Console.WriteLine("Scale = {0}", scale);
+#else
+			System.Diagnostics.Debug.WriteLine("Scale = {0}", scale);
+#endif
+#endif
+            canvas.Scale(scale);
+
+            using (var stream = GetType().Assembly.GetManifestResourceStream(@"XamarinFormsSVGdemo.black_cat.svg"))
+            using(var paint = new SKPaint())
+            {
+                var svg = new SkiaSharp.Extended.Svg.SKSvg();
+                svg.Load(stream);
+
+                canvas.DrawPicture(svg.Picture, paint);
+            }
+
         }
     }
 }
